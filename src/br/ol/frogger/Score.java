36cbe9fd5b6cf8065bc166b0ee83f5d5bd87df1a
Unknown <60010421@kmitl.ac.th>
2018-04-17 23:00:41 +0700

    update class score

--- src/br/ol/frogger/Score.java
+++ src/br/ol/frogger/Score.java
@@ -4,11 +4,16 @@
  * and open the template in the editor.
  */
 package br.ol.frogger;
-
+import java.util.Scanner;
+import java.io.*;
 /**
  *
  * @author N8NNY
  */
 public class Score {
+    private int hiscore;
     
+    public void Score(int hiscore) {
+        this.hiscore = hiscore;
+    }
 }
